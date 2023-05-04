d3e04e51f91e7142b3689a8ffb3bd6971428a368
vontell <vontell@mit.edu>
2023-05-04 10:13:44 -0400

    New changes

--- index.js
+++ index.js
@@ -4,23 +4,6 @@ export function configureBot() {
 
 export async function runTurn(tickInfo, mostRecentMatchInfo, actionQueue) {
 
-  let allObjects = Object.values(tickInfo)
-  let humanPlayer = allObjects.find(o => o.type === "HumanPlayer")
-  let botPlayer = allObjects.find(o => o.type === "BotPlayer")
-  if (humanPlayer && botPlayer) {
-    console.log(`Received ${allObjects.length} object states`)
-    console.log("HUMAN PLAYER\n", humanPlayer)
-    console.log("BOT PLAYER\n", botPlayer)
-    // Example logic - if the bot is at least 5 away from the player, move towards the player
-    let hp = humanPlayer.position;
-    let bp = botPlayer.position;
-    let desiredRange = 5;
-    let distance = Math.sqrt(Math.pow(hp.x - bp.x, 2) + Math.pow(hp.y - bp.y, 2) + Math.pow(hp.z - bp.z, 2));
-    console.log(`Distance is ${distance}, and range is ${desiredRange}`);
-    if (distance > desiredRange) {
-      console.log("Sending action");
-      actionQueue.queue("FollowObject", {targetId: humanPlayer.id, range: 5})
-    }
-  }
+  console.log(tickInfo)
 
 }
