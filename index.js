29a64f2167e409612d310d597bb1c12b68679c6a
RG-nAmKcAz <112959031+RG-nAmKcAz@users.noreply.github.com>
2023-05-05 15:21:08 -0400

    Make mage bot better

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const RGBot = {
 
 const BossRoomBot = {
 
-  abilities: [0, 1, 2],
+  // mage only has 2 abilities, other characterTypes may have more
+  abilities: [0, 1],
 
   getEnemies: (tickInfo) => {
     return RGBot.getEntitiesOnTeam(tickInfo, 1);
   },
+  
+  getAllies: (tickInfo) => {
+    return RGBot.getEntitiesOnTeam(tickInfo, 0);
+  }
 
   startAbility: (ability, position, targetId, actionQueue) => {
     const input = {
@@ -44,15 +49,20 @@ export async function runTurn(tickInfo, mostRecentMatchInfo, actionQueue) {
 
   console.log(`Running 'runTurn' with new tickInfo`)
 
-  // Collect some initial information that is needed
-  const enemies = BossRoomBot.getEnemies(tickInfo);
-  console.log(`Found ${enemies.length} enemies!`);
-
-  // Some abilities require an enemy id and position
-  const randomEnemy = enemies[Math.floor(Math.random() * enemies.length)];
+  // Some abilities require an enemy/ally id and position
   const ability = BossRoomBot.abilities[CURRENT_ABILITY % BossRoomBot.abilities.length];
   console.log(`Trying out ability ${ability}`);
-  BossRoomBot.startAbility(ability, randomEnemy.position, randomEnemy.id, actionQueue);
+  if (ability % BossRoomBot.abilities.length == 0) {
+      const enemies = BossRoomBot.getEnemies(tickInfo);
+      console.log(`Found ${enemies.length} enemies!`);
+      const randomEnemy = enemies[Math.floor(Math.random() * enemies.length)];
+      BossRoomBot.startAbility(ability, randomEnemy.position, randomEnemy.id, actionQueue);
+  } else {
+      const allies = BossRoomBot.getAllies(tickInfo);
+      console.log(`Found ${allies.length} allies!`);
+      const randomAlly = allies[Math.floor(Math.random() * allies.length)];
+      BossRoomBot.startAbility(ability, randomAlly.position, randomAlly.id, actionQueue);
+  }
   CURRENT_ABILITY++;
 
 } 
