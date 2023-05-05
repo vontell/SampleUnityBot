8f3ea90f5f8649a89dff31cec846e52f7ceb1a61
RG-nAmKcAz <zack@regression.gg>
2023-05-05 16:36:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const BossRoomBot = {
     }
     actionQueue.queue("PerformSkill", input)
   }
-//test
+
 }
 
 class RGValidator {
@@ -46,8 +46,9 @@ class RGValidator {
 
 let charType = Math.round(Math.random() * 1000000) % 4;
 
-export function configureBot() {
-  console.log(`Unity bot configureBot function called, charType: ${charType} !`)
+export function configureBot(characterType) {
+  console.log(`Unity bot configureBot function called, charType: ${charType} - characterType: ${characterType}`)
+  charType = characterType
 }
 
 
