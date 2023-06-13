dfa6ace68eb41d2f4e3401cbab718171275878b7
abeizer <abby@regression.gg>
2023-06-13 14:44:55 -0400

    log tickInfo

--- validatorConfigureBot/index.js
+++ validatorConfigureBot/index.js
@@ -2,21 +2,39 @@ import {BossRoomBot, CharInfo} from "../bossroom";
 import {RGValidator} from "../rg";
 
 let rgValidator = new RGValidator();
+let charType = 1; // fixed to rogue character
+let botComplete = false;
 
-let charType = Math.round(Math.random() * 1000000) % 4;
-
-let bot;
-
-export function configureBot(qaBot) {
-  bot = qaBot;
-  console.log(`onfigureBot called, charType: ${bot.characterType}`);
-  charType = CharInfo.type.indexOf(bot.characterType);
+// flags for clicking the 6 buttons we need to click to start the game
+let stateFlags = {
+  "RGHostButton":false,
+  "StartWithRGButton":false,
+  "SelectProfileButton":false,
+  "ProfileMenuButton":false,
+  "ReadyButton":false,
+  "Seat7Button":false,
 }
 
-export async function runTurn(playerId, tickInfo, mostRecentMatchInfo, actionQueue) {
+export function configureBot(bot) {
+  console.log(`BOTCODE: charTypeWas: ${bot.characterType}`);
+  charType = CharInfo.type.indexOf(bot.characterType);
 
-  console.log(`Tick number ${tickInfo.tick}: ${bot.getState()}`);
-  
+}
+
+
+// function getEntitiesByType(state, objectType) {
+//   const entities = Object.values(state.gameState).filter(e => e.type === type);
+// }
+
+
+// function waitForScene(bot) {
+//   const sceneName = bot.getState().sceneName;
+
+// }
+
+
+export async function runTurn(bot) {
+  console.log(`BOTCODE Tick number ${tickInfo.tick}: ${JSON.stringify(bot.getState())}`);
 }
 
 /**
@@ -25,3 +43,4 @@ export async function runTurn(playerId, tickInfo, mostRecentMatchInfo, actionQue
 export function getCharacterType() {
   return CharInfo.type[charType];
 }
+
