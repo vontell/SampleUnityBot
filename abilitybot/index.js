186f8888247e65355cddd6f72101b560e53f9906
vontell <vontell@mit.edu>
2023-07-08 20:22:23 -0400

    new test code

--- abilitybot/index.js
+++ abilitybot/index.js
@@ -14,8 +14,8 @@ let charType = Math.round(Math.random() * 1000000) % 4;
 export function configureBot(rgObject) {
    const characterType = JSON.parse(rgObject.characterConfig).characterType;
    if (characterType) {
-     console.log(`Unity bot configureBot function called, charType: ${charType} - characterType: ${characterType}`);
      charType = CharInfo.type.indexOf(rgObject.characterType);
+     console.log(`Unity bot configureBot function called, charType: ${charType} - characterType: ${characterType}`);
    }
 }
 
